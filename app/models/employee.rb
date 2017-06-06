8bb914e6664448b7f56833eab3f2720ca600f0b7
tracer11 <karolgurba22@gmail.com>
2017-06-05 21:31:46 -0500

    initial commit

---
+++ app/models/employee.rb
@@ -0,0 +1,8 @@
+class Employee < ApplicationRecord
+  has_many :addresses
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/employees' => 'employees#index'
+  post '/employees' => 'employees#create'
+  get '/employees/:id' => 'employees#show'
+
+end
